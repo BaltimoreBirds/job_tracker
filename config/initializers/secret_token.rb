155d0679c65341544c5fcc207201953303b327b8
Michael Swanson <mikeswanson12@msn.com>
2014-04-18 15:37:58 -0400

    moves database.yml to example file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JobTracker::Application.config.secret_key_base = '1e20ea42ddca48b8b32ae1a9dcfe96d987c0d74e45cf4f35959b433a389eadfbb1823d448dea3cd5a8765b3420edd64cdd7a75699d114a488fa7d68a6842a1bb'
