7da885f400f5ac3a99836256c26c26e11acaac8d
Michael Swanson <mikeswanson12@msn.com>
2014-04-19 15:39:45 -0400

    adds webmock gem, tests that external requests are blocked

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,10 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rails'
 require 'capybara/rspec'
+require 'webmock/rspec'
+
+
+WebMock.disable_net_connect!(allow_localhost: true)
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
