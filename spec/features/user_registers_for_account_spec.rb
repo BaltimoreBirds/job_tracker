3368cf20ed49f74ccb08f559a7e77fab62817398
Michael Swanson <mikeswanson12@msn.com>
2014-04-19 14:55:06 -0400

    creating feature tests for user sign up

---
+++ spec/features/user_registers_for_account_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+features 'unregistered user registers for account', %Q{
+	As an unregistered user
+	I want to register
+	So that my jobs will be saved. 
+
+	Acceptance Critera
+	-unregistered users see register button on home page.
+	- 
+} do
+	
+end
