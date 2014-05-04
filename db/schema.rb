959719f98de28554776ddd2ac8f50d7348c11de2
Michael Swanson <mikeswanson12@msn.com>
2014-05-04 13:09:16 -0400

    create user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140504165727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: true do |t|
+    t.string   "github_uid",   null: false
+    t.string   "name",         null: false
+    t.string   "display_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
