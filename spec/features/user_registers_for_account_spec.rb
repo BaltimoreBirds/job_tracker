b85b17647b8cf4d5c1219cf73b2f4569fd49ba42
Michael Swanson <mikeswanson12@msn.com>
2014-05-06 14:18:28 -0400

    reconfigurr vcr.rb

--- spec/features/user_registers_for_account_spec.rb
+++ spec/features/user_registers_for_account_spec.rb
@@ -11,13 +11,15 @@ features 'unregistered user registers for account', %Q{
 	- upon registration, user is taken to user job board page. 
 } do
 
-	scenario 'user sees home page, and registration link' do
+	scenario 'New user signs in with github' do
+
+		use_vcr_cassette "feed", :record => :new_episodes
+
 
 		prev_count = User.count
-
 		visit '/' 
 		expect(page).to have_content('Job Tracker')
-		click_on 'Sign up'
+		click_on 'Sign in with Github'
 
 		expect(page).to have_content('Sign up with your Github Account.')
 		fill_in "Email", with: 
