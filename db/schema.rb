bd57e70c99baffa472193f99695d11de250fe11e
Michael Swanson <mikeswanson12@msn.com>
2014-06-03 14:55:21 -0400

    removes job_users table migration files

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140603182209) do
+ActiveRecord::Schema.define(version: 20140603185213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "job_users", force: true do |t|
-    t.integer  "job_id"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "jobs", force: true do |t|
     t.string   "title",       null: false
