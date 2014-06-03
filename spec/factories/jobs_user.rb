16285a8a4c8785f8a133d65798d5494140255983
Michael Swanson <mikeswanson12@msn.com>
2014-06-03 16:38:28 -0400

    corrects factory girl jobs_user naming

--- spec/factories/jobs_user.rb
+++ spec/factories/jobs_user.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :jobs_user, :class => 'JobsUsers' do
+  factory :jobs_user, :class => 'JobsUser' do
     job_id 1
     user_id 1
   end
